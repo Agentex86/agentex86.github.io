2d258c3b9f99c4af616fdd6085dec90342d00e71
Felipe Lunkes <felipenldev@gmail.com>
2023-11-02 02:23:21 -0300

    Update

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -1,5 +1,5 @@
 Jekyll::Hooks.register :posts, :post_write do |post|
-    all_existing_tags = Dir.entries("_tags")
+    all_existing_tags = Dir.entries("tags")
     .map { |t| t.match(/(.*).md/) }
     .compact.map { |m| m[1] }
 
@@ -11,11 +11,11 @@ end
 
 def generate_tag_file(tag)
     # generate tag file
-    File.open("_tags/#{tag}.md", "wb") do |file|
+    File.open("tags/#{tag}.md", "wb") do |file|
     file << "---\nlayout: tags\ntag-name: #{tag}\n---\n"
     end
     # generate feed file
-    File.open("_feeds/#{tag}.xml", "wb") do |file|
+    File.open("feeds/#{tag}.xml", "wb") do |file|
     file << "---\nlayout: feed\ntag-name: #{tag}\n---\n"
     end
 end
